4db5c8b72fb68e677bffe1db0bb279d73fe28991
liushuyan <1497365285@qq.com>
2018-11-27 15:34:45 +0800

    修改后提交

--- app/src/main/java/com/liu/gitdemo/MainActivity.java
+++ app/src/main/java/com/liu/gitdemo/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //首页信息
+        //分类信息
     }
 }
